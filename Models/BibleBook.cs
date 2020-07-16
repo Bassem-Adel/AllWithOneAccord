330ddc89f7f16bbdcdede4b51e71b7e524e25eff
Bassem Adel <Basem.adel1995@gmail.com>
2020-07-16 22:03:35 +0200

    initial Book Reading

---
+++ Models/BibleBook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllWithOneAccord.Models
+{
+    public class BibleBook
+    {
+        public string Name { get; set; }
+
+        public BookChapter[] BookChapters { get; set; }
+    }
+
+    public class BookChapter
+    {
+        public long Number { get; set; }
+
+        public string ChapterIntro { get; set; }
+        public string ChapterTailing { get; set; }
+
+        public ChapterAudio[] Audios { get; set; }
+        
+        public Verse[] Verses { get; set; }
+    }
+
+    public class ChapterAudio
+    {
+        public int ChapterNumber { get; set; }
+
+        public string ChapterReader { get; set; }
+
+        public string ChapterPath { get; set; }
+
+        public string ChapterSplitting { get; set; }
+    }
+
+    public class Verse
+    {
+        public long Number { get; set; }
+
+        public string Text { get; set; }
+    }
+}
